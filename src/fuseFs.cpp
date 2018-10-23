054b3e58cf782f95a5638a96cfa11fdce7d4731d
Otavio Cardoso Borges <otavio@oleivos.SSG5-Serial>
2018-10-22 20:54:14 -0400

    Draft for sftp filesystem functions

--- src/fuseFs.cpp
+++ src/fuseFs.cpp
@@ -1,26 +1,95 @@
-#define FUSE_USE_VERSION 26
+#define FUSE_LOG_ID     "FUSE"
 
 #include <stdio.h>
 #include <string.h>
 #include <string.h>
 #include <errno.h>
+#include <libssh/sftp.h>
 #include "fuseFs.h"
+#include "LogInfo.h"
 using namespace dromedary;
+
+static void *FuseThread(void *arg){
+    if(arg == NULL){
+        return NULL;
+    }
+
+    struct fuse *fs = (struct fuse *)arg;
+    if (fuse_loop(fs) < 0) {
+        // signal error
+        return NULL;
+    }
+
+    return NULL;
+}
+
+static int fuseGetAttr(const char *path, struct stat *stbuf);
+static int fuseReadDir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi);
+static int fuseOpen(const char *path, struct fuse_file_info *fi);
+static int fuseRead(const char *path, char *buf, size_t size, off_t offset, struct fuse_file_info *fi);
+
+Sftp                *gManager = NULL;
+char                *gMountPoint = NULL;
+pthread_t           gFuseThread;
+
+struct fuse_chan    *gChannel;
+struct fuse         *gFuse;
+static struct fuse_operations operations;
+
+void InitFuse(Sftp *manager){
+    LogInfo::AddEntry(FUSE_LOG_ID, "Setting default SFTP manager", LogLevel::DEBUG);
+    gManager = manager;
+
+    operations.getattr = fuseGetAttr;
+    operations.open    = fuseOpen;
+    operations.read    = fuseRead;
+    operations.readdir = fuseReadDir;
+}
+
+bool Mount(char *mountpoint){
+    if(gManager == NULL){
+        LogInfo::AddEntry(FUSE_LOG_ID, "Cannot mount without a defined SFTP manager", LogLevel::ERROR);
+        return false;
+    }
+
+    gMountPoint = new char[strlen(mountpoint)];
+    strcpy(gMountPoint, mountpoint);
+
+    gChannel = fuse_mount(gMountPoint, NULL);
+    gFuse = fuse_new(gChannel, NULL, &operations, sizeof(operations), NULL);
+    pthread_create(&gFuseThread, NULL, FuseThread, (void *)gFuse);
+
+    return true;
+}
+
+void Unmount(void){
+    fuse_destroy(gFuse);
+    fuse_unmount(gMountPoint, gChannel);
+}
 
 static int fuseGetAttr(const char *path, struct stat *stbuf){
     int response = 0;
-
-    memset(stbuf, 0, sizeof(struct stat));
-    if(strcmp(path, "/") == 0){
-        stbuf->st_mode = S_IFDIR | 0755;
-        stbuf->st_nlink = 2;
-    }else if(strcmp(path+1, "hello") == 0){
-        stbuf->st_mode = S_IFREG | 0444;
-        stbuf->st_nlink = 1;
-        stbuf->st_size = 6;
-    }else{
+    sftp_attributes at = sftp_stat(*gManager->GetSFTPSession(), path);
+    if(at == NULL){
         response = -ENOENT;
+    }else{
+        memset(stbuf, 0, sizeof(struct stat));
+        stbuf->st_mode = (at->type << 16) | at->permissions;
+        stbuf->st_nlink = 1;
+        stbuf->st_size = at->size;
     }
+    
+    // memset(stbuf, 0, sizeof(struct stat));
+    // if(strcmp(path, "/") == 0){
+    //     stbuf->st_mode = S_IFDIR | 0755;
+    //     stbuf->st_nlink = 2;
+    // }else if(strcmp(path+1, "hello") == 0){
+    //     stbuf->st_mode = S_IFREG | 0444;
+    //     stbuf->st_nlink = 1;
+    //     stbuf->st_size = 6;
+    // }else{
+    //     response = -ENOENT;
+    // }
 
     return response;
 }
@@ -28,13 +97,16 @@ static int fuseGetAttr(const char *path, struct stat *stbuf){
 static int fuseReadDir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi){
     (void) offset;
     (void) fi;
+    sftp_dir dir;
+    sftp_attributes attibutes;
 
-    if(strcmp(path, "/") != 0)
+    dir = sftp_opendir(*gManager->GetSFTPSession(), path);
+    if(!dir)
         return -ENOENT;
-    
-    filler(buf, ".", NULL, 0);
-    filler(buf, "..", NULL, 0);
-    filler(buf, "hello", NULL, 0);
+
+    while((attibutes = sftp_readdir(*gManager->GetSFTPSession(), dir)) != NULL){
+        filler(buf, attibutes->name, NULL, 0);
+    }
 
     return 0;
 }
@@ -67,32 +139,4 @@ static int fuseRead(const char *path, char *buf, size_t size, off_t offset, stru
     }
 
     return size;
-}
-
-FuseFileSystem::FuseFileSystem(Sftp *sftp, SftpFunctions *functions){
-    m_sftp = sftp;
-    m_functions = functions;
-}
-bool FuseFileSystem::Mount(char *path){
-    m_mountPoint = new char[strlen(path)];
-    strcpy(m_mountPoint, path);
-
-    struct fuse_operations oper;
-    oper.getattr = fuseGetAttr;
-    oper.readdir = fuseReadDir;
-    oper.open    = fuseOpen;
-    oper.read    = fuseRead;
-
-    m_channel = fuse_mount(m_mountPoint, NULL);
-    m_session = fuse_new(m_channel, NULL, &oper, sizeof(oper), NULL);
-    fuse_loop(m_session);
-
-    return true;
-}
-
-bool FuseFileSystem::Unmount(void){
-    fuse_destroy(m_session);
-    fuse_unmount(m_mountPoint, m_channel);
-
-    return true;
 }
--- src/fuseFs.h
+++ src/fuseFs.h
@@ -1,23 +1,14 @@
 #ifndef SRC_FUSE_H_
 #define SRC_FUSE_H_
 
+#define FUSE_USE_VERSION 26
+
+#include <pthread.h>
 #include <fuse.h>
 #include "sftp.h"
-#include "sftpFunctions.h"
 
-namespace dromedary{
-    class FuseFileSystem{
-        private:
-            Sftp *m_sftp;
-            SftpFunctions *m_functions;
-            char *m_mountPoint;
-            struct fuse_chan *m_channel;
-            struct fuse *m_session;
-        public:
-            FuseFileSystem(Sftp *sftp, SftpFunctions *functions);
-            bool Mount(char *path);
-            bool Unmount(void);
-    };
-}
+void InitFuse(dromedary::Sftp *manager);
+bool Mount(char *mountpoint);
+void Unmount(void);
 
 #endif // SRC_FUSE_H_
